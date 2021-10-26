7fd8e4da0aea2f4a05896e2ab38b2f67c4f71b58
Wan Ashraf <73482535+Ashraf-wan@users.noreply.github.com>
2021-10-26 18:24:34 +0800

    Create Installer.py

---
+++ Installer.py
@@ -0,0 +1,17 @@
+def loop():
+  choice = input("Please choose your desktop environment.(lxde, lxqt, none, xfce, kde)")
+
+  if choice == "lxde":
+    os.system("docker run -it parrotsecurity/corvid:lxde-0.5")
+  elif choice == "lxqt":
+    os.system("docker run -it parrotsecurity/corvid:lxqt-0.5")
+  elif choice == "none":
+    os.system("docker run -it parrotsecurity/corvid:slim-0.5")
+  elif choice == "xfce":
+    os.system("docker run -it parrotsecurity/corvid:xfce-0.5")
+  elif choice == "kde":
+    os.system("docker run -it parrotsecurity/corvid:kde-0.5")
+  else:
+    print("Wrong input")
+    loop()
+loop()
